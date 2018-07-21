c1c25efefdac04e18b2ea3cc385362f4f66f8e61
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-07-22 01:45:05 +0530

    Initial commit: Project structure with libraries added

---
+++ chessAI.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	var board,
+	  game = new Chess();
+
+	// do not pick up pieces if the game is over
+	// only pick up pieces for White
+	var onDragStart = function(source, piece, position, orientation) {
+	  if (game.in_checkmate() === true || game.in_draw() === true ||
+	    piece.search(/^b/) !== -1) {
+	    return false;
+	  }
+	};
+
+	var calculateBestMove = function() {
+	  var possibleMoves = game.moves();
+
+	  // game over
+	  if (possibleMoves.length === 0) return;
+
+	  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+	  game.move(possibleMoves[randomIndex]);
+	  board.position(game.fen());
+	};
+
+	var onDrop = function(source, target) {
+	  // see if the move is legal
+	  var move = game.move({
+	    from: source,
+	    to: target,
+	    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+	  });
+
+	  // illegal move
+	  if (move === null) return 'snapback';
+
+	  // make random legal move for black
+	  window.setTimeout(calculateBestMove, 250);
+	};
+
+	// update the board position after the piece snap
+	// for castling, en passant, pawn promotion
+	var onSnapEnd = function() {
+	  board.position(game.fen());
+	};
+
+	var cfg = {
+	  draggable: true,
+	  position: 'start',
+	  onDragStart: onDragStart,
+	  onDrop: onDrop,
+	  onSnapEnd: onSnapEnd
+	};
+	board = ChessBoard('board', cfg);
+});
